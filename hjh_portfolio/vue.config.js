3eb0310debe367e5baf9c55cec2cb9b4e23a80a8
huiseon <legavin1023@gmail.com>
2025-04-10 15:06:38 +0900

    이름 실수

--- hjh_portfolio/vue.config.js
+++ hjh_portfolio/vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/html_automation_task/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/HJH_Portfolio/" : "/",
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
